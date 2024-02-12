20049a1042d49a7d2c9ea2f1213ba7ce0560835c
Gabriele Gonçalves <109491188+gabrieleglvs@users.noreply.github.com>
2024-02-12 01:38:37 -0300

    ajustando erro de digitacao

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import app from "./src/app.js"
 
 const PORT = 5000;
 
-//aqui eu estou abrindo minha conexão com o servidor na porta 3001
+//aqui eu estou abrindo minha conexão com o servidor na porta 5000
 app.listen(PORT, () => {
     console.log("Servidor escutando!")
 });
