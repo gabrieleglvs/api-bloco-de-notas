764c7f6c4ad848529cbe08332e6e6e0f65613d39
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2024-02-11 23:00:21 -0300

    adicionando dependencia 'cors'

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import nota from "./notaRouter.js"
+import cors from "cors";
 
 const routes = (app) => {
     app.route('/').get((req, res) => 
         res.status(200).send('TODO LIST - Minha primeira API em Node')
     )
 
+    app.use(cors());
     app.use(express.json(), nota);
 }
 
