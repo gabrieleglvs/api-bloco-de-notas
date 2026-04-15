0f36a43e0a5275f0cc6fa834d8a4791f03a690e1
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2026-04-15 15:50:31 -0300

    fix: script de inicializacao e porta do servidor

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./src/app.js"
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //aqui eu estou abrindo minha conexão com o servidor na porta 5000
 app.listen(PORT, () => {
